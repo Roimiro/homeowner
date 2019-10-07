1212e289fd342e949d69706e4a6b62cfcfe7ae24
Roimiro <miroroi@yahoo.com>
2019-10-07 13:43:58 +0300

    data modle message

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Message from '../components/message'
+import Parse from 'parse'
 
 class Messages extends React.Component {
     render(){
+       
         return(
             <Message/>
         )
