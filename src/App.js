766cff731487e51048c456ba7555114a2b914b45
Roimiro <miroroi@yahoo.com>
2019-10-07 10:44:06 +0300

    adding app routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
 
 class App extends React.Component {
   render (){
     return(
-      <div> hello world</div>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path='/signup'>
+          <Signup/>
+        </Route>
+        <Routh path='/dashboardcommitte'>
+          <DashboardCommitte/>
+        </Routh>
+        <Route path='/dashboardtenants'>
+          <DashboardTenats/>
+        </Route>
+        <Route path='/tenants'>
+        <Tenants/>
+        </Route>
+        <Route path='/messages'>
+          <Messages/>
+        </Route>
+        <Route path='/newmessage'>
+          <NewMessage/>
+        </Route>
+        <Routh path='/votings'>
+          <Votings/>
+        </Routh>
+        <Routh path='/newvoting'>
+          <NewVoting/>
+        </Routh>
+      </Switch>
     )
   }
 }
