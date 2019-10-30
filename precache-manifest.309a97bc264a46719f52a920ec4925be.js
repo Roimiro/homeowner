2f9d53d753da708111129b18e6025fbaa67224aa
Roimiro <miroroi@yahoo.com>
2019-10-30 13:07:55 +0200

    Updates

---
+++ precache-manifest.309a97bc264a46719f52a920ec4925be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553f62f2a7771a32347bef35dde060eb",
+    "url": "/homeowner/index.html"
+  },
+  {
+    "revision": "9daa98e9b49b584c4ab0",
+    "url": "/homeowner/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fd11d6e9fdc8274728d9",
+    "url": "/homeowner/static/css/main.c6e4c32f.chunk.css"
+  },
+  {
+    "revision": "9daa98e9b49b584c4ab0",
+    "url": "/homeowner/static/js/2.c469f05a.chunk.js"
+  },
+  {
+    "revision": "fd11d6e9fdc8274728d9",
+    "url": "/homeowner/static/js/main.5de165ce.chunk.js"
+  },
+  {
+    "revision": "a8b4d48d4eae7c21c633",
+    "url": "/homeowner/static/js/runtime-main.e5a489f0.js"
+  },
+  {
+    "revision": "5edee41a9a967d44dc54acae38fd0859",
+    "url": "/homeowner/static/media/house.5edee41a.jpeg"
+  }
+]);
